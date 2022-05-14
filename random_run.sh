4cfb976bd777b0210ebddb901b458910570d8db0
Anurag Kar <anuragkar09@gmail.com>
2022-05-13 23:03:58 -0400

    script changes

--- random_run.sh
+++ random_run.sh
@@ -13,7 +13,7 @@ l2_large_num_set=2048
 /data/anurag/macsim/macsim_internal/tools/run_batch_local.py -bin /data/anurag/macsim/bin/macsim -param /data/anurag/macsim/bin/params.in -suite spec17 -dir 'spec17__mode-'$llc_remapping_mode'__interval-'$llc_remapping_interval'__numllc-'$num_llc'__llcnumset-'$llc_num_set'__l2numset-'$l2_large_num_set'__insts-'$max_insts -cmd 'max_insts='$max_insts' --llc_remapping_mode='$llc_remapping_mode' --llc_remapping_interval='$llc_remapping_interval' --num_llc='$num_llc' --llc_num_set='$llc_num_set' --l2_large_num_set='$l2_large_num_set
 #####################################################
 #####################################################
-#To find working set 
+#To find working set p
 llc_remapping_mode=none
 llc_num_set=2048
 l2_large_num_set=256
